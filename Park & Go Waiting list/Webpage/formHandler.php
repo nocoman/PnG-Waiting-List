8db72d2b65b76143103d25ae14f984338f03229d
Noah C <nocoman5@gmail.com>
2016-06-02 09:35:03 -0500

    Added files
    i added some files

---
+++ Park & Go Waiting list/Webpage/formHandler.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php
+    $firstName = null;
+    $lastName =  null;
+    $phoneNumber;
+    
+    if($firstName == "Noah"){
+        echo "Hello Noah";
+    }
+    
+	echo $_POST['firstName'];
+?>
+
+</body>
+</html>
